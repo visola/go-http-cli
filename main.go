8cbd3f080111fd989d71d9d6ffa53c342e546e7b
Vinicius Isola <viniciusisola@gmail.com>
2017-09-26 16:44:19 -0400

    Use gofmt to format code

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "os"
-  "strings"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 )
 
 type headerFlags []string
@@ -16,78 +16,76 @@ func (i *headerFlags) String() string {
 }
 
 func (i *headerFlags) Set(value string) error {
-  *i = append(*i, value)
-  return nil
+	*i = append(*i, value)
+	return nil
 }
 
-func main () {
-  var method string
-  var headers headerFlags
+func main() {
+	var method string
+	var headers headerFlags
 
-  flag.StringVar(&method, "method", "GET", "HTTP method to be used")
-  flag.Var(&headers, "header", "Headers to include with your request")
+	flag.StringVar(&method, "method", "GET", "HTTP method to be used")
+	flag.Var(&headers, "header", "Headers to include with your request")
 
-  flag.Parse()
+	flag.Parse()
 
-  fmt.Println("Method: ", method)
+	fmt.Println("Method: ", method)
 
-  if len(flag.Args()) != 1 {
-    fmt.Println("Nothing to do.")
-    os.Exit(1)
-  }
+	if len(flag.Args()) != 1 {
+		fmt.Println("Nothing to do.")
+		os.Exit(1)
+	}
 
-  url := flag.Args()[0]
-  fmt.Println("URL: ", url)
+	url := flag.Args()[0]
+	fmt.Println("URL: ", url)
 
-  req, reqErr := http.NewRequest("GET", url, nil)
+	req, reqErr := http.NewRequest("GET", url, nil)
 
-  if reqErr != nil {
-    fmt.Println("Error while creating request: ", reqErr)
-    os.Exit(10)
-  }
+	if reqErr != nil {
+		fmt.Println("Error while creating request: ", reqErr)
+		os.Exit(10)
+	}
 
-  for _, kv := range headers {
-    s := strings.Split(kv, "=")
-    if len(s) != 2 {
-      fmt.Println("Error while parsing header: ", kv)
-      fmt.Println("Should be a '=' separated key/value, e.g.: Content-type=application/x-www-form-urlencoded")
-      os.Exit(11)
-    }
-    req.Header.Add(s[0], s[1])
-  }
+	for _, kv := range headers {
+		s := strings.Split(kv, "=")
+		if len(s) != 2 {
+			fmt.Println("Error while parsing header: ", kv)
+			fmt.Println("Should be a '=' separated key/value, e.g.: Content-type=application/x-www-form-urlencoded")
+			os.Exit(11)
+		}
+		req.Header.Add(s[0], s[1])
+	}
 
-  client := &http.Client {}
-  resp, respErr := client.Do(req)
+	client := &http.Client{}
+	resp, respErr := client.Do(req)
 
-  if respErr != nil {
-    fmt.Println("There was an error.")
-    fmt.Println(respErr)
-    os.Exit(20)
-  }
+	if respErr != nil {
+		fmt.Println("There was an error.")
+		fmt.Println(respErr)
+		os.Exit(20)
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  // fmt.Println(req)
+	for k, v := range req.Header {
+		fmt.Printf(">> '%s' = '%s'\n", k, v)
+	}
+	fmt.Println("---")
 
-  for k, v := range req.Header {
-    fmt.Printf(">> '%s' = '%s'\n", k, v)
-  }
-  fmt.Println("---")
+	// fmt.Println(resp)
+	for k, v := range resp.Header {
+		fmt.Printf("<< '%s' = '%s'\n", k, v)
+	}
 
-  // fmt.Println(resp)
-  for k, v := range resp.Header {
-    fmt.Printf("<< '%s' = '%s'\n", k, v)
-  }
+	fmt.Println("---")
 
-  fmt.Println("---")
+	bodyBytes, readErr := ioutil.ReadAll(resp.Body)
 
-  bodyBytes, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		fmt.Println("Error while reading body.", readErr)
+		os.Exit(30)
+	}
 
-  if readErr != nil {
-    fmt.Println("Error while reading body.", readErr)
-    os.Exit(30)
-  }
-
-  fmt.Println(string(bodyBytes))
+	fmt.Println(string(bodyBytes))
 
 }
