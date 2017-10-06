2f8bd8ae3e54a5e4dec377d80e4c11f6e08d45e7
Vinicius Isola <viniciusisola@gmail.com>
2017-10-06 17:44:51 -0400

    Missed gofmt in one file

--- config/hierarchical_configuration.go
+++ config/hierarchical_configuration.go
@@ -6,40 +6,40 @@ type hierarchicalConfigurationFormat struct {
 
 func (conf hierarchicalConfigurationFormat) Headers() map[string][]string {
 	result := make(map[string][]string)
-  for _, subConfig := range conf.configurations {
-    for k, v := range subConfig.Headers() {
-      result[k] = v
-    }
-  }
-  return result
+	for _, subConfig := range conf.configurations {
+		for k, v := range subConfig.Headers() {
+			result[k] = v
+		}
+	}
+	return result
 }
 
 func (conf hierarchicalConfigurationFormat) Body() string {
-  var result string
-  for _, subConfig := range conf.configurations {
-    if subConfig.Body() != "" {
-      result = subConfig.Body()
-    }
-  }
+	var result string
+	for _, subConfig := range conf.configurations {
+		if subConfig.Body() != "" {
+			result = subConfig.Body()
+		}
+	}
 	return result
 }
 
 func (conf hierarchicalConfigurationFormat) Method() string {
-  var result string
-  for _, subConfig := range conf.configurations {
-    if subConfig.Method() != "" {
-      result = subConfig.Method()
-    }
-  }
+	var result string
+	for _, subConfig := range conf.configurations {
+		if subConfig.Method() != "" {
+			result = subConfig.Method()
+		}
+	}
 	return result
 }
 
 func (conf hierarchicalConfigurationFormat) URL() string {
-  var result string
-  for _, subConfig := range conf.configurations {
-    if subConfig.URL() != "" {
-      result = subConfig.URL()
-    }
-  }
+	var result string
+	for _, subConfig := range conf.configurations {
+		if subConfig.URL() != "" {
+			result = subConfig.URL()
+		}
+	}
 	return result
 }
