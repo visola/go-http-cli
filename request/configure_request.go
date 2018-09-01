53c4b44542b10399834d5beafd618e504e8cd25d
Vinicius Isola <viniciusisola@gmail.com>
2018-09-01 18:23:54 -0400

    Add configure request method

---
+++ request/configure_request.go
@@ -0,0 +1,59 @@
+package request
+
+import (
+	"fmt"
+
+	"github.com/visola/go-http-cli/profile"
+)
+
+// ConfigureRequest configures a request to be executed based on the provided options
+func ConfigureRequest(unconfiguredRequest Request, requestName string, executionOptions ExecutionOptions) (*Request, error) {
+	mergedProfile, profileError := loadAndMergeProfiles(executionOptions.ProfileNames)
+	if profileError != nil {
+		return nil, profileError
+	}
+
+	requestOptions, requestOptionsErr := findNamedRequest(mergedProfile, requestName)
+	if requestOptionsErr != nil {
+		return nil, requestOptionsErr
+	}
+
+	configuredRequest := Request{}
+
+	configuredRequest.Merge(mergedProfile)
+	configuredRequest.Merge(requestOptions)
+	configuredRequest.Merge(unconfiguredRequest)
+
+	configuredRequest.URL = ParseURL(mergedProfile.BaseURL, configuredRequest.URL, requestOptions.URL)
+
+	return &configuredRequest, nil
+}
+
+func findNamedRequest(mergedProfile profile.Options, requestName string) (profile.RequestOptions, error) {
+	if requestName == "" {
+		return profile.RequestOptions{}, nil
+	}
+
+	var requestOptions profile.RequestOptions
+
+	var exists bool
+	if requestOptions, exists = mergedProfile.RequestOptions[requestName]; requestName != "" && !exists {
+		return profile.RequestOptions{}, fmt.Errorf("Request with name %s not found", requestName)
+	}
+
+	return requestOptions, nil
+}
+
+func loadAndMergeProfiles(profileNames []string) (profile.Options, error) {
+	profiles := make([]profile.Options, len(profileNames))
+
+	for index, profileName := range profileNames {
+		loadedProfile, profileError := profile.LoadProfile(profileName)
+		if profileError != nil {
+			return profile.Options{}, profileError
+		}
+		profiles[index] = loadedProfile
+	}
+
+	return profile.MergeOptions(profiles), nil
+}
--- request/parse_url.go
+++ request/parse_url.go
@@ -4,8 +4,9 @@ import (
 	"strings"
 )
 
-// ParseURL parses the configuration to generate the final URL that the request will be sent to.
-func ParseURL(base string, path string) string {
+// ParseURL parses the configuration to generate the final URL that the request will be sent to
+func ParseURL(base string, paths ...string) string {
+	path := coalesce(paths...)
 	url := path
 
 	if !strings.HasPrefix(url, "http") && base != "" {
@@ -21,4 +22,14 @@ func ParseURL(base string, path string) string {
 	}
 
 	return url
+}
+
+func coalesce(values ...string) string {
+	for _, oneValue := range values {
+		if oneValue != "" {
+			return oneValue
+		}
+	}
+
+	return ""
 }
