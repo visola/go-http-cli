fe4999340dad8ccba1fae0bbf77fde002d8d82ff
Vinicius Isola <viniciusisola@gmail.com>
2019-01-17 22:13:10 -0500

    Add headers on method tests

--- integrationtests/methods_test.go
+++ integrationtests/methods_test.go
@@ -29,19 +29,27 @@ func testMethod(t *testing.T, method string) {
 	userID := "1234"
 	companyID := "7890"
 	timestamp := "20181201083035"
+	value2 := "Some Value for a Variable"
 	path := "/users/{userID}?timestamp={timestamp}"
 
 	RunHTTP(
 		t,
+		"-H", "Authorization: Basic QXJlIHlvdSBraWRkaW5nIG1lPw==",
+		"-H", "Multi-Value=value1",
+		"-H", "Multi-Value={value2}",
 		"-V", "companyID="+companyID,
 		"-V", "userID="+userID,
 		"-V", "timestamp="+timestamp,
+		"-V", "value2="+value2,
 		"-X", method,
 		testServer.URL+path,
 		"companyID={companyID}",
 	)
 
 	HasMethod(t, lastRequest, method)
+	HasHeader(t, lastRequest, "Authorization", "Basic QXJlIHlvdSBraWRkaW5nIG1lPw==")
+	HasHeader(t, lastRequest, "Multi-Value", "value1")
+	HasHeader(t, lastRequest, "Multi-Value", value2)
 	HasPath(t, lastRequest, "/users/"+userID)
 	HasQueryParam(t, lastRequest, "timestamp", timestamp)
 
