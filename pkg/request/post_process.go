ead8bde8eed03d7dc08f20ea0cd77646a5e91471
Vinicius Isola <viniciusisola@gmail.com>
2019-09-12 07:18:16 -0400

    Add basic way to add requests to be executed

--- pkg/request/post_process.go
+++ pkg/request/post_process.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/robertkrimen/otto"
+	"github.com/visola/go-http-cli/pkg/session"
 )
 
 // PostProcessContext stores
 type PostProcessContext struct {
-	Output string
+	Output   string
+	Requests []Request
 }
 
 // PostProcessSourceCode represents the source code to be executed
@@ -21,32 +23,42 @@ type PostProcessSourceCode struct {
 }
 
 // PostProcess processes the executed requests using the post processing script
-func PostProcess(executionContext *ExecutionContext, executedRequests []ExecutedRequestResponse, responseErr error) (string, error) {
+func PostProcess(executionContext *ExecutionContext, executedRequests []ExecutedRequestResponse, responseErr error) (*PostProcessContext, error) {
 	sourceCode := executionContext.PostProcessCode
 	if sourceCode.SourceCode == "" {
-		return "", nil
+		return &PostProcessContext{}, nil
 	}
 
 	vm := otto.New()
 
 	script, compileErr := vm.Compile(sourceCode.SourceFilePath, sourceCode.SourceCode)
 	if compileErr != nil {
-		return "", compileErr
+		return &PostProcessContext{}, compileErr
 	}
 
 	context := preparePostProcessContext(vm, executionContext, executedRequests, responseErr)
 
 	_, executeError := vm.Run(script)
 	if executeError != nil {
-		return "", executeError
+		return &PostProcessContext{}, executeError
 	}
 
-	return context.Output, nil
+	return context, nil
 }
 
 func createAddVariableFunction(executionContext *ExecutionContext) func(string, string) {
 	return func(name string, value string) {
-		executionContext.Session.Variables[name] = value
+		session.SetVariable(executionContext.Session.Host, name, value)
+	}
+}
+
+func createAddRequestFunction(context *PostProcessContext) func(otto.Value) {
+	return func(value otto.Value) {
+		if value.IsString() {
+			context.Requests = append(context.Requests, Request{
+				URL: otto.Value.String(value),
+			})
+		}
 	}
 }
 
@@ -64,10 +76,12 @@ func createPrintlnFunction(context *PostProcessContext) func(...interface{}) {
 
 func preparePostProcessContext(vm *otto.Otto, executionContext *ExecutionContext, executedRequests []ExecutedRequestResponse, responseErr error) *PostProcessContext {
 	context := &PostProcessContext{
-		Output: "",
+		Output:   "",
+		Requests: make([]Request, 0),
 	}
 
 	vm.Set("addVariable", createAddVariableFunction(executionContext))
+	vm.Set("addRequest", createAddRequestFunction(context))
 	vm.Set("print", createPrintFunction(context))
 	vm.Set("println", createPrintlnFunction(context))
 
