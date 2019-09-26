12d3a9658107353b5a129544296815a83196e9bf
Vinicius Isola <viniciusisola@gmail.com>
2019-09-25 21:36:33 -0400

    fix(build): Fix upload artifacts to release

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
+BASE_PACKAGE=build/packages/
+mkdir -p $BASE_PACKAGE
+
 build_and_zip() {
     echo "Building and packaging for $1-$2"
     # $1 -> operating system
     # $2 -> architecture
     # $3 -> OS alias, used in the output file name
     # $4 -> Optional extension with ".", e.g.: .exe
-    PACKAGE_DIR=build/$3_$2
+    PACKAGE_DIR=$BASE_PACKAGE/$3_$2
     mkdir -p $PACKAGE_DIR
 
     # build http cli
@@ -26,7 +29,7 @@ build_and_zip() {
     cp LICENSE $PACKAGE_DIR/
     cp README.md $PACKAGE_DIR/
 
-    zip -j build/go-http-cli_${VERSION}_$3_$2.zip $PACKAGE_DIR/*
+    zip -j $BASE_PACKAGE/go-http-cli_${VERSION}_$3_$2.zip $PACKAGE_DIR/*
     rm -Rf $PACKAGE_DIR
 }
 
--- scripts/release.sh
+++ scripts/release.sh
@@ -6,9 +6,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ./semantic-release --token $GITHUB_TOKEN --slug visola/go-http-cli --ghr --vf
 export VERSION=$(cat .version)
 
-ls -la
-cat .version
-cat .ghr
-
 $SCRIPT_DIR/package.sh
-ghr $(cat .ghr) build/*.zip
+ghr $(cat .ghr) build/packages
