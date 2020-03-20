7f430f925b75929c5d17f40580ec39f8c8cd0c10
Vinicius Isola <viniciusisola@gmail.com>
2020-03-20 08:08:03 -0400

    chore(build): fix release script to use travis.com

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-./semantic-release --token $GITHUB_TOKEN --slug visola/go-http-cli --ghr --vf
+./semantic-release --token $GITHUB_TOKEN -travis-com --slug visola/go-http-cli --ghr --vf
 export VERSION=$(cat .version)
 
 $SCRIPT_DIR/package.sh
